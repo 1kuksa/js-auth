d7dadf15af6c60e5e73be4e59c961f041b35828b
Oleksandr Kuksa <sasha_kuksa29@ukr.net>
2023-11-05 14:20:24 +0200

    js-auth part2

--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  role: 1,
+})
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -48,5 +54,27 @@ router.get('/signup', function (req, res) {
   // ↑↑ сюди вводимо JSON дані
 })
 
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля не заповненні",
+    })
+  }
+
+  try {
+    User.create({ email, password, role })
+    return res.status(200).json({
+      message: 'Користувач успішно зареєстрованний',
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Помилка. Створення користувача',
+    })
+  }
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
